ea82e6788cb9e4b7ee3d4131641483f8b04cb874
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-21 15:23:37 -0500

    Se agrego la funcion del programa y se termino todo con pruebas

--- MatrizPrimero.c
+++ MatrizPrimero.c
@@ -9,6 +9,25 @@ int main()
     scanf("%d", &n);
     printf("Ingeres el numero de columnas:\n");
     scanf("%d", &m);
+    int Matrix[n][m];
+    printf("La matriz es: \n");
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j)
+            {
+                Matrix[i][j] = 1;
+            }
+            else
+            {
+                Matrix[i][j] = 0;
+            }
+            printf("%d\t", Matrix[i][j]);
+        }
+    }
 
     return 0;
 }
