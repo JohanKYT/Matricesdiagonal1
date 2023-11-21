676538bcfa314d3182b34e989ecc7e65c149d775
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-21 15:22:11 -0500

    Agregamos las variables e iniciamos el main para que corra el progreso

--- MatrizPrimero.c
+++ MatrizPrimero.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int main()
+{
 
+    int n, m;
+    printf("Ingeres el numero de filas:\n");
+    scanf("%d", &n);
+    printf("Ingeres el numero de columnas:\n");
+    scanf("%d", &m);
+
+    return 0;
+}
